70a12c9acf197e91381fd6df3bef26d5b979d139
Maxim <zinkov.maxim@gmail.com>
2016-04-08 14:38:59 +0500

    Hello Jetty Server

---
+++ src/main/java/frontend/Frontend.java
@@ -0,0 +1,13 @@
+package frontend;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello Jetty Server!");
+    }
+}
